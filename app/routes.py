d06fe0fced4ece4d5ca1fe3ccea5d8295606ec7b
Marco Heins <marco.heins@posteo.de>
2018-08-19 16:32:41 +0200

    input name and display it

---
+++ app/routes.py
@@ -0,0 +1,15 @@
+from flask import render_template
+
+from app import app
+from app.forms import NameForm
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    name = None
+    form = NameForm()
+
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('index.html', form=form, name=name)
