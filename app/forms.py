99f5087002bfb3a38aa8dcdb33bd4539475f34df
Marco Heins <marco.heins@posteo.de>
2018-08-21 22:36:58 +0200

    +password length check, +macro print_red(errors)

--- app/forms.py
+++ app/forms.py
@@ -1,11 +1,12 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField, BooleanField
-from wtforms.validators import DataRequired, Email
+from wtforms.validators import DataRequired, Email, Length
 
 
 class UserForm(FlaskForm):
     email = StringField('Email:',
                         validators=[Email('Please enter a valid email address'), DataRequired()])
-    password = PasswordField('Password:', validators=[DataRequired()])
+    password = PasswordField('Password:',
+                             validators=[DataRequired(), Length(min=8)])
     remember_me = BooleanField('Remember me')
     submit = SubmitField('Sign in')
