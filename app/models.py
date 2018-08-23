8174399863eae06c0b9bf3808d31cefa0b9ffe16
Marco Heins <marco.heins@posteo.de>
2018-08-23 22:46:22 +0200

    hello_name.py: +make_shell_context(), + .flaskenv

--- app/models.py
+++ app/models.py
@@ -20,4 +20,4 @@ class Post(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     def __repr__(self):
-        return f'<Post {self.body}'
+        return f'<Post {self.body}>'
