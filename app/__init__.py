13a41271b32963746483576f9ed94bd4a24529f5
Marco Heins <marco.heins@posteo.de>
2018-08-23 21:58:13 +0200

    +models.py & migrations/

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask_debugtoolbar import DebugToolbarExtension
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 from config import Config
 
@@ -7,6 +9,9 @@ from config import Config
 app = Flask(__name__)
 app.config.from_object(Config)
 toolbar = DebugToolbarExtension(app)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-from app import routes
+
+from app import routes, models
 
